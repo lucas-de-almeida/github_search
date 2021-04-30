a39e375cddcb15ac63b6498a79d928af028b5f4c
Almeida <lalmeida6@PMINTL.NET>
2021-04-30 05:52:57 -0300

    adicionada camada datasource

---
+++ test/modules/search/external/datasources/github_datasource_test.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:github_search/modules/search/domain/errors/errors.dart';
+import 'package:github_search/modules/search/external/datasources/github_datasource.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../utils/github_response.dart';
+
+class DioMock extends Mock implements Dio {}
+
+main() {
+  final dio = DioMock();
+  final datasource = GithubDatasource(dio);
+  test("deve retornar uma instancia de ResultSearchModel", () async {
+    when(dio.get(any)).thenAnswer(
+        (_) async => Response(data: jsonDecode(githubResult), statusCode: 200));
+
+    final result = datasource.getSearch('searchText');
+    expect(result, completes);
+  });
+
+  test("deve retornar um erro se o codigo nao for 200", () async {
+    when(dio.get(any))
+        .thenAnswer((_) async => Response(data: null, statusCode: 401));
+
+    final future = datasource.getSearch('searchText');
+    expect(future, throwsA(isA<DatasourceError>()));
+  });
+
+  test("deve retornar umException se tiver um erro no DIO", () async {
+    when(dio.get(any)).thenThrow(Exception());
+
+    final future = datasource.getSearch('searchText');
+    expect(future, throwsA(isA<Exception>()));
+  });
+}
